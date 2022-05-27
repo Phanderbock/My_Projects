80dddf3e3e01e62fd4d1979964fb93d2fdedc0b0
Phanderbock <brennangallagher2256@gmail.com>
2022-05-26 20:05:49 -0400

    Tags framework
    Framework for tags, not yet fully functional.

--- Python/Django/mysite/blog/views.py
+++ Python/Django/mysite/blog/views.py
@@ -5,6 +5,7 @@ from django.core.paginator import Paginator, EmptyPage,\
 from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from taggit.models import Tag
 # Create your views here.
 
 
@@ -15,8 +16,13 @@ class PostListView(ListView):
     template_name = 'blog/post/list.html'
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     object_list = Post.published.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags_in=[tag])
     paginator = Paginator(object_list, 3)  # 3 posts in each page
     page = request.GET.get('page')
     try:
@@ -30,7 +36,8 @@ def post_list(request):
     return render(request,
         'blog/post/list.html',
          {'page': page,
-        'posts': posts})
+        'posts': posts,
+        'tag': tag})
 
 
 def post_detail(request, year, month, day, post):
