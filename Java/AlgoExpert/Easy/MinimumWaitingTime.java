012b80b98c073e5141b9f7130849c72c0e11fb71
Phanderbock <brennangallagher2256@gmail.com>
2022-06-21 11:24:56 -0400

    AlgoExpert.io code.
    Added resolutions to three more problems.

---
+++ Java/AlgoExpert/Easy/MinimumWaitingTime.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+
+public class MinimumWaitingTime {
+
+	public int minimumWaitingTime(int[] queries) {
+		Arrays.sort(queries);
+		
+		int totalWaitingTime = 0;
+		for (int idx = 0; idx < queries.length; idx++) {
+			int duration = queries[idx];
+			int queriesLeft = queries.length - (idx + 1);
+			totalWaitingTime += duration * queriesLeft;
+		}
+		
+		return totalWaitingTime;
+	}
+}
