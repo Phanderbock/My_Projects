4f45aae00d1e9d8e639a2f3ef8e43352baf8a694
Phanderbock <brennangallagher2256@gmail.com>
2022-06-24 09:32:17 -0400

    Another four exercises

---
+++ Java/AlgoExpert/Easy/ProductSum.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+
+public class ProductSum {
+	
+	public static int productSum(List<Object> array) {
+		return productSumHelper(array, 1);
+	}
+	
+	public static int productSumHelper(List<Object> array, int multiplier) {
+		int sum = 0;
+		for (Object el : array) {
+			if (el instanceof ArrayList) {
+				@SuppressWarnings("unchecked")
+				ArrayList<Object> ls = (ArrayList<Object>) el;
+				sum += productSumHelper(ls, multiplier + 1);
+			} else {
+				sum += (int) el;
+			}
+		}
+		
+		return sum * multiplier;
+	}
+
+}
