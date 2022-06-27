8d4e1cbc90614061cd97fcd02768e1d1c1a24ccc
Phanderbock <brennangallagher2256@gmail.com>
2022-06-27 09:31:49 -0400

    Final "easy" algoexpert

--- Java/AlgoExpert/Easy/FirstNonrepeatingCharacter.java
+++ Java/AlgoExpert/Easy/FirstNonrepeatingCharacter.java
@@ -1,4 +1,20 @@
+import java.util.*;
 
 public class FirstNonrepeatingCharacter {
-
+	public int firstNonRepeatingCharacter(String string) {
+		HashMap<Character, Integer> characterFrequencies = new HashMap<Character, Integer>();
+		
+		for (int idx = 0; idx < string.length(); idx++) {
+			char character = string.charAt(idx);
+			characterFrequencies.put(character, characterFrequencies.getOrDefault(character, 0) + 1);	
+		}
+		
+		for (int idx = 0; idx < string.length(); idx++) {
+			char character = string.charAt(idx);
+			if (characterFrequencies.get(character) == 1) {
+				return idx;
+			}
+		}
+		return -1;
+	}
 }
