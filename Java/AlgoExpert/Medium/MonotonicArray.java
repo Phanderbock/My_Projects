8f25819979cd30fc55e9408f60532047f37f1bab
Phanderbock <brennangallagher2256@gmail.com>
2022-07-20 09:40:36 -0400

    AlgoExpert Medium
    Batch of 4 medium solutions.

---
+++ Java/AlgoExpert/Medium/MonotonicArray.java
@@ -0,0 +1,17 @@
+
+public class MonotonicArray {
+
+	public static boolean isMonotonic(int[] array) {
+		var isNonDecreasing = true;
+		var isNonIncreasing = true;
+		for (int i = 1; i < array.length; i++) {
+			if (array[i] < array[i - 1]) {
+				isNonDecreasing = false;
+			}
+			if (array[i] > array[i - 1]) {
+				isNonIncreasing = false;
+			}
+		}
+		return isNonDecreasing || isNonIncreasing;
+	}
+}
