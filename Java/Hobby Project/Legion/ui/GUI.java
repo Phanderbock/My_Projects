c575040dbebee051d7eeacfaaf9ba99424995614
Phanderbock <brennangallagher2256@gmail.com>
2022-09-14 12:02:01 -0400

    Adding Logos for GUI

--- Java/Hobby Project/Legion/ui/GUI.java
+++ Java/Hobby Project/Legion/ui/GUI.java
@@ -1,29 +1,36 @@
 package ui;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
-public class GUI extends JFrame{
+public class GUI extends JFrame {
 
-	ImageIcon sepsPic = new ImageIcon("\\Legion Reference Docs\\Separatists\\CIS.jpeg");
+	BufferedImage Logo1 = ImageIO.read(new File("C:/Users/Brennan/Documents/Github/My_Projects/Java/Hobby Project/Legion/ui/CIS.png"));
+	ImageIcon sepsPic = new ImageIcon(Logo1);
+	BufferedImage Logo2 = ImageIO.read(new File("C:/Users/Brennan/Documents/Github/My_Projects/Java/Hobby Project/Legion/ui/GAR.png"));
+	ImageIcon GARPic = new ImageIcon(Logo2);
+	BufferedImage Logo3 = ImageIO.read(new File("C:/Users/Brennan/Documents/Github/My_Projects/Java/Hobby Project/Legion/ui/Empire.png"));
+	ImageIcon ImpPic = new ImageIcon(Logo3);
+	BufferedImage Logo4 = ImageIO.read(new File("C:/Users/Brennan/Documents/Github/My_Projects/Java/Hobby Project/Legion/ui/Rebels.png"));
+	ImageIcon RebsPic = new ImageIcon(Logo4);
 	JButton seps = new JButton("Separatists", sepsPic);
-	JButton seps2 = new JButton(sepsPic);
 	// Constructor to setup the GUI components and event handlers
-	public GUI() {
+	public GUI() throws IOException{
 		super("Star Wars Legion by Phanderbock");
 		setSize(600, 600);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	    JPanel panel = new JPanel();
 	    panel.add(seps);
-		panel.add(seps2);
 	    add(panel);
 	    setVisible(true);
 	}
  
 	// The entry main() method
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		GUI ui = new GUI();
-		
-
 	}
 
 
