ea8eff4d87224152ea4a3e374c1964978419a6f4
Phanderbock <brennangallagher2256@gmail.com>
2022-09-17 11:46:46 -0400

    Added icons, initialized button classes

---
+++ Java/Hobby Project/Legion/ui/SepsButton.java
@@ -0,0 +1,11 @@
+package ui;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+
+public class SepsButton extends JFrame {
+
+}
