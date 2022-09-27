32341e2b7d41c9063cec7d21307f7286957601d9
Phanderbock <brennangallagher2256@gmail.com>
2022-09-27 11:11:22 -0400

    Create SepsButton.java
    Initial lists to be used for class button functions.

---
+++ Java/Hobby Project/Legion/ui/SepsButton.java
@@ -0,0 +1,14 @@
+package ui;
+
+public class SepsButton {
+    String[] CommanderList = new String[] {"General Grievous", "Count Dooku", "Kraken", "Kalani", "Super Tactical Droid", "T-Series Tactical Droid"};
+    String[] OperativeList = new String[] {"Darth Maul", "Cad Bane", "Bossk"};
+    String[] CorpList = new String[] {"B1 Battle Droids", "B2 Super Battle Droids"};
+    String[] SFList = new String[] {"BX-Series Droid Commandos Strike Team", "BX-Series Droid Commandos", "IG-100 MagnaGuard"};
+    String[] SupportList = new String[] {"Droidekas", "DSD1 Dwarf Spider Droid", "STAP Riders"};
+    String[] HeavyList = new String[] {"AAT Trade Federation Battle Tank", "NR-N99 Persuader-class Tank Droid"};
+
+    public SepsButton(){
+        
+    }
+}
