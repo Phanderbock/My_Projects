732c48b69e006f7044f3a59bccabac009b2d7eab
Phanderbock <brennangallagher2256@gmail.com>
2022-10-05 10:01:51 -0400

    Update UICardLayout.java
    Removed unused import.

--- Java/Hobby Project/Legion/ui/UICardLayout.java
+++ Java/Hobby Project/Legion/ui/UICardLayout.java
@@ -7,7 +7,6 @@ import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
 
 public class UICardLayout implements ItemListener{
 
