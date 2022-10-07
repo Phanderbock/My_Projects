cdc278f36ae4bbc3f9462684669465a87e136ed2
Phanderbock <brennangallagher2256@gmail.com>
2022-10-07 11:14:41 -0400

    Points
    Points button now displays point costs for each unit.

--- Java/Hobby Project/KharadronOverlords/KharadronClass.java
+++ Java/Hobby Project/KharadronOverlords/KharadronClass.java
@@ -18,6 +18,15 @@ public class KharadronClass implements ItemListener {
 
         JPanel coverPanel = new JPanel();
         JPanel buttonsPanel = new JPanel();
+        JButton goBack = new JButton("Go Back");
+        goBack.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                basePanel.removeAll();
+                basePanel.add(buttonsPanel);
+                basePanel.repaint();
+                basePanel.validate();
+            }
+        });
         JButton coverButton = new JButton(coverIcon);
         coverButton.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e){
@@ -28,15 +37,45 @@ public class KharadronClass implements ItemListener {
         });
 
         JButton points = new JButton("Points");
+        points.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                JPanel pointsPanel = new JPanel();
+                JLabel pointsLabel = new JLabel(
+                    "<html>Unit Size  /  Unit Name     / Points / Role / Extra Notes<br> <br>"
+                    + "     (10) Arkanaut Company     /  90  / Battleline / <br>"
+                    + "     (1) Arkanaut Frigate     / 250 /   Behemoth  / Battleline in Barak-Zilfin  <br>"
+                    + "    (1) Arkanaut Ironclad    / 490 /   Behemoth  / <br>"
+                    + "     (1) Aether-Khemist      / 90  /    Leader   / <br>"
+                    + "     (1) Aetheric Navigator   / 100 /    Leader   / <br>"
+                    + "   (1) Arkanaut Admiral     / 140 /    Leader   / <br>"
+                    + "    (1) Brokk Gurngsson    / 240 /    Leader   / Unique  <br>"
+                    + " (1) Endrinmaster w/ D. Suit / 220 /    Leader   / <br>"
+                    + " (1) Endrinmaster w/ Harness / 100 /    Leader   / <br>"
+                    + "   (1) Bjorgen Thundrik    / 140 /    Leader   / <br>"
+                    + " (4) Thundrik's Profiteers  / n/a /             / Unique, must be taken with Bjorgen <br>"
+                    + "    (3)  Endrinriggers      / 100 /             / Battleline if Endrinmaster w/ D.Suit is General <br>"
+                    + " (1) Grundstock Gunhauler   / 150 /             / Battleline in Barak-Urbaz <br>"
+                    + " (5) Grundstock Thunderers  / 120 /             / Battleline in Barak-Nar <br>"
+                    + "   (3)   Skywardens         / 100 /             / Battleline if Endrinmaster w/ D.Suit is General </html"
+                    );
+                pointsPanel.add(goBack);
+                pointsPanel.add(pointsLabel);
+                basePanel.add(pointsPanel);
+                basePanel.remove(buttonsPanel);
+            }
+        });
 
         JButton warscrolls = new JButton("Warscrolls");
 
         JButton battalions = new JButton("Battalions");
 
+        JButton pathToGlory = new JButton("Path to Glory");
+
         coverPanel.add(coverButton, BorderLayout.NORTH);
         buttonsPanel.add(points);
         buttonsPanel.add(warscrolls);
         buttonsPanel.add(battalions);
+        buttonsPanel.add(pathToGlory);
 
         basePanel = new JPanel(new CardLayout());
         basePanel.add(coverPanel);
